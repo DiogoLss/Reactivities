b0100c54f8fd228c7d0c6c54e257ad14282ba916
Diogo Lessa <diogodslessa@gmail.com>
2022-11-27 13:57:17 -0300

    cors and app react workin

--- API/Startup.cs
+++ API/Startup.cs
@@ -37,6 +37,11 @@ namespace API
             services.AddDbContext<DataContext>(opt => {
                 opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(options => {
+                options.AddPolicy("CorsPolicy", policy => {
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,7 +56,9 @@ namespace API
 
             // app.UseHttpsRedirection();
 
-            app.UseRouting();
+            app.UseRouting(); 
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
