6fc3e2574a7df5a52c47b636f30374e602b5bea9
Diogo Lessa <diogodslessa@gmail.com>
2022-12-05 21:57:00 -0300

    Create and Edit implemented

--- Application/Activities/Details.cs
+++ Application/Activities/Details.cs
@@ -19,16 +19,13 @@ namespace Application.Activities
             private DataContext _context;
             public Handler(DataContext context)
             {
-            this._context = context;
+            this._context = context; 
                 
             }
             public async Task<Activity> Handle(Query request, CancellationToken cancellationToken)
             {
                 return await _context.Activities.FindAsync(request.Id);
             }
-        }
-        {
-
         }
     }
 }
