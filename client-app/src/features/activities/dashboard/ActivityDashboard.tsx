63bbd4ec14fc47d68960408defa763f8cd02523d
DiogoLss <diogodslessa@gmail.com>
2022-12-20 16:34:25 -0300

    CRUD client side finished

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,17 +1,47 @@
 import React from 'react';
-import { Grid, List } from 'semantic-ui-react';
+import { Grid } from 'semantic-ui-react';
 import { Activity } from '../../../app/models/activity';
+import ActivityDetail from '../details/ActivityDetails';
+import ActivityForm from '../form/ActivityForm';
 import ActivityList from './ActivityList';
 
 interface Props{
     activities: Activity[];
+    selectedActivity: Activity | undefined;
+    editMode: boolean;
+    selectActivity: (id: string) => void;
+    cancelSelectActivity: () => void;
+    openForm: (id: string) => void;
+    closeForm: () => void;
+    createOrEdit: (activity: Activity) => void;
+    deleteActivity: (id: string) => void;
 }
 
-export default function ActivityDashboard({activities}: Props){
+export default function ActivityDashboard({activities, selectedActivity, editMode,
+        cancelSelectActivity, selectActivity,openForm,closeForm, createOrEdit, deleteActivity}: Props){
     return(
         <Grid>
-            <Grid.Column>
-                <ActivityList activities={activities} />
+            <Grid.Column width='10'>
+                <ActivityList 
+                    activities={activities} 
+                    selectActivity={selectActivity}
+                    deleteActivity={deleteActivity}
+                />
+            </Grid.Column>
+            <Grid.Column width='6'>
+                {selectedActivity && !editMode &&
+                <ActivityDetail 
+                    activity={selectedActivity} 
+                    cancelSelectActivity={cancelSelectActivity}
+                    openForm={openForm}
+                />}
+                {editMode &&
+                    <ActivityForm 
+                        activity={selectedActivity} 
+                        closeForm={closeForm} 
+                        createOrEdit={createOrEdit} 
+                    />
+                }
             </Grid.Column>
         </Grid>
     )
